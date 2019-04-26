e3b38fcdf2bcb140c517155951643f2bf13bc5bd
Brendan <abae97@gmail.com>
2019-04-26 12:59:05 -0400

    starting report

--- experiments.py
+++ experiments.py
@@ -1,6 +1,5 @@
-from Q1.Q1 import main as q1
-from Q2.Q2 import q2_experiment as q2
-from Q3.Q3 import q3_experiment as q3
-from Q4.Q4 import q4_experiment as q4
-from Q5.Q5 import q5_experiment as q5
+from Q1.Q1 import Q1 as q1
+from Q2.Q2 import Q2_timing as q2
+from Q5.Q5 import Q5_output as q5
+from Q6.Q6 import Q6 as q6
 
